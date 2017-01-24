bfd72938a0559ebbd463bd38c2a4c638919ea900
LeastCreative <evenlesscreative@gmail.com>
2017-01-24 12:31:32 -0600

    Initial Commit

---
+++ html/src/com/supershooter/game/client/HtmlLauncher.java
@@ -0,0 +1,19 @@
+package com.supershooter.game.client;
+
+import com.badlogic.gdx.ApplicationListener;
+import com.badlogic.gdx.backends.gwt.GwtApplication;
+import com.badlogic.gdx.backends.gwt.GwtApplicationConfiguration;
+import com.supershooter.game.SuperShooter;
+
+public class HtmlLauncher extends GwtApplication {
+
+        @Override
+        public GwtApplicationConfiguration getConfig () {
+                return new GwtApplicationConfiguration(480, 320);
+        }
+
+        @Override
+        public ApplicationListener createApplicationListener () {
+                return new SuperShooter();
+        }
+}
