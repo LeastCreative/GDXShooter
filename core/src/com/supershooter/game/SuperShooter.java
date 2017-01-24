1bbe1e2cdef8e0e3cdba878546b43f84e8bc4f48
LeastCreative <evenlesscreative@gmail.com>
2017-01-24 16:59:32 -0600

    Stupid Test Screen

--- core/src/com/supershooter/game/SuperShooter.java
+++ core/src/com/supershooter/game/SuperShooter.java
@@ -2,32 +2,104 @@ package com.supershooter.game;
 
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
-import com.badlogic.gdx.graphics.GL20;
-import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.*;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
+
+import static com.badlogic.gdx.graphics.glutils.ShapeRenderer.*;
 
 public class SuperShooter extends ApplicationAdapter {
-	SpriteBatch batch;
-	Texture img;
-	
-	@Override
-	public void create () {
-		batch = new SpriteBatch();
-		img = new Texture("badlogic.jpg");
-	}
+    SpriteBatch batch;
+    Texture img;
+    ShapeRenderer shapes;
+    Camera camera;
+    Square[] squares;
 
-	@Override
-	public void render () {
-		Gdx.gl.glClearColor(1, 0, 0, 1);
-		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-		batch.begin();
-		batch.draw(img, 0, 0);
-		batch.end();
-	}
-	
-	@Override
-	public void dispose () {
-		batch.dispose();
-		img.dispose();
-	}
+    @Override
+    public void create() {
+        camera = new OrthographicCamera(1280, 720);
+        batch = new SpriteBatch();
+        batch.setProjectionMatrix(camera.combined);
+        shapes = new ShapeRenderer();
+        shapes.setProjectionMatrix(camera.combined);
+        img = new Texture("super-shooter.png");
+        squares = new Square[98];
+        for (int i = 0; i < squares.length; i++) {
+            squares[i] = new Square(-640, 40 * i);
+        }
+    }
+
+    @Override
+    public void render() {
+        Gdx.gl.glClearColor(1, 1, 1, 1);
+        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+
+        //draw sprites to sprite renderer
+        batch.begin();
+        batch.draw(img, 0 - img.getWidth() / 2, 0);
+        batch.end();
+
+        //draw squares to shape renderer
+        shapes.begin(ShapeType.Filled);
+        shapes.setColor(1, 0, 0, 1);
+        for (int i = 0; i < squares.length; i++) {
+            squares[i].update();
+            squares[i].draw(shapes);
+        }
+        shapes.end();
+    }
+
+    void updateRect() {
+    }
+
+    @Override
+    public void dispose() {
+        batch.dispose();
+        img.dispose();
+    }
+}
+
+
+class Square {
+    int x;
+    int y;
+    int speed = 5;
+
+    //0:up, 1:right, 2:down, 3:left
+    int direction = 0;
+
+
+    public Square(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public void draw(ShapeRenderer target) {
+        target.rect(x, y, 20, 20);
+    }
+
+    public void update() {
+        switch (direction) {
+            case 0:
+                y -= speed;
+                if (y <= -360)
+                    direction++;
+                break;
+            case 1:
+                x += speed;
+                if (x >= 620)
+                    direction++;
+                break;
+            case 2:
+                y += speed;
+                if (y >= 340)
+                    direction++;
+                break;
+            case 3:
+                x -= speed;
+                if (x <= -640)
+                    direction = 0;
+                break;
+        }
+    }
 }
--- desktop/src/com/supershooter/game/desktop/DesktopLauncher.java
+++ desktop/src/com/supershooter/game/desktop/DesktopLauncher.java
@@ -5,8 +5,15 @@ import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
 import com.supershooter.game.SuperShooter;
 
 public class DesktopLauncher {
-	public static void main (String[] arg) {
-		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
-		new LwjglApplication(new SuperShooter(), config);
-	}
+    public static void main(String[] arg) {
+        LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+        config.width = 1280;
+        config.height = 720;
+        config.title = "Test";
+        config.fullscreen = false;
+        config.foregroundFPS = 60;
+        config.forceExit = true;
+        config.vSyncEnabled = false;
+        new LwjglApplication(new SuperShooter(), config);
+    }
 }
