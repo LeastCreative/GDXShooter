7b170de3c578bc138c73c8e4e481b4d55cd6111c
LeastCreative <evenlesscreative@gmail.com>
2017-01-29 22:09:54 -0600

    Implemented basic hud, gives score at random

---
+++ core/src/com/supershooter/game/Hud.java
@@ -0,0 +1,58 @@
+package com.supershooter.game;
+
+import com.badlogic.gdx.graphics.Color;
+import com.badlogic.gdx.graphics.g2d.BitmapFont;
+import com.badlogic.gdx.scenes.scene2d.Stage;
+import com.badlogic.gdx.scenes.scene2d.ui.Label;
+import com.badlogic.gdx.scenes.scene2d.ui.Table;
+
+/**
+ * The information about the game displayed during play
+ * <p>
+ * Created by Dan on 1/29/2017.
+ */
+class Hud extends Stage {
+
+    private int score;
+    private int lives;
+
+    private Label scoreLabel;
+    private Label livesLabel;
+
+    Hud() {
+        super();
+        score = 1220;
+        lives = 5;
+
+        Table table = new Table();
+        table.top();
+        table.moveBy(60, 40);
+        scoreLabel = new Label(String.format("%6d", score), new Label.LabelStyle(new BitmapFont(), Color.WHITE));
+        livesLabel = new Label(lives + "", new Label.LabelStyle(new BitmapFont(), Color.WHITE));
+
+        //set up score row
+        table.row();
+        table.add(new Label("SCORE", new Label.LabelStyle(new BitmapFont(), Color.WHITE)));
+        table.add(new Label(" : ", new Label.LabelStyle(new BitmapFont(), Color.WHITE)));
+        table.add(scoreLabel);
+
+        //set up lives rolls
+        table.row();
+        table.add(new Label("LIVES", new Label.LabelStyle(new BitmapFont(), Color.WHITE)));
+        table.add(new Label(" : ", new Label.LabelStyle(new BitmapFont(), Color.WHITE)));
+        table.add(livesLabel);
+
+        this.addActor(table);
+    }
+
+    void addPoints(int points) {
+        this.score += points;
+        scoreLabel.setText(score + "");
+    }
+
+    void addLives(int lives) {
+        this.lives += lives;
+        livesLabel.setText(score + "");
+    }
+
+}
